ab4aa30581c2b9c62bae4e8136edd632ac46d13c
Даниил Ханин <morgotleninsinx@gmail.com>
2026-06-22 18:29:21 +0300

    fix: explicit path for module import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
